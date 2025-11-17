f6070c8ebb9c2b61336ac052a013dcbf4683ccee
Ranelabs <ranelabs@outlook.com>
2025-11-17 16:04:54 -0500

    Update index.js

--- api/GetVisitorCount/index.js
+++ api/GetVisitorCount/index.js
@@ -1,23 +1,15 @@
 module.exports = async function (context, req) {
-  // Log that the function was called (shows in Azure logs)
-  context.log("GetVisitorCount function called");
+    context.log("GetVisitorCount function called");
 
-  // Hard-coded value just to verify things work
-  const count = 1;
+    const count = 1; // test value
 
-  // Build the response object
-  const response = {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: {
-      count: count
-    }
-  };
-
-  context.log("Sending response:", response);
-
-  // Return the response
-  return response;
+    context.res = {
+        status: 200,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: {
+            count: count
+        }
+    };
 };
