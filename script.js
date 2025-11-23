e4ddfb8405b6a5b1f76b5d052c46090790f50c9d
Ranelabs <ranelabs@outlook.com>
2025-11-22 21:45:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function updateCounter() {
-    const response = await fetch("YOUR_AZURE_FUNCTION_URL");
+    const response = await fetch("https://nice-pond-00275090f.3.azurestaticapps.net/api/GetVisitorCount");
     const count = await response.text();
     document.getElementById("counter").innerText = count;
 }
